9c720c127343ff4fbf5a435856e88e3e1df45b57
Ivan-A-ZabalaMontano <ivanzabalamontano@gmail.com>
2022-06-01 17:40:35 -0400

    agregando routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CirugiasComponent } from './cirugias/cirugias.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
+import { PlantelComponent } from './plantel/plantel.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'',redirectTo:'/home',pathMatch:'full'
+  },
+  {
+    path:'servicios',component:ServiciosComponent
+  },
+  {
+    path:'plantel',component:PlantelComponent
+  },
+  {
+    path:'reservas',component:ReservasComponent
+  },
+  {
+    path:'cirugias',component:CirugiasComponent
+  },
+  {
+    path:'contacto',component:ContactoComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PlantelComponent } from './plantel/plantel.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { CirugiasComponent } from './cirugias/cirugias.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServiciosComponent,
+    PlantelComponent,
+    ReservasComponent,
+    CirugiasComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
